443d5fe5c4c36e9c01ead3285b1d87e81cfaac07
Antonino Cucchiara <antonio_cuc@yahoo.co.uk>
2011-03-28 11:03:44 +0100

    Added gpx test suite.

---
+++ test/gpxsplitter/GpxSplitterTestSuite.java
@@ -0,0 +1,42 @@
+package gpxsplitter;
+
+/**
+ * This suite contains all the tests for the GpxSplitter.
+ *
+ * @author Antonino Cucchiara
+ */
+
+import gpxsplitter.Tools.GpxRouteLoaderTest;
+import gpxsplitter.Tools.GpxTrackLoaderTest;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({GpxRouteLoaderTest.class, GpxTrackLoaderTest.class})
+public class GpxSplitterTestSuite {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception
+    {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception
+    {
+    }
+
+    @Before
+    public void setUp() throws Exception
+    {
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+    }
+
+}
