54346078f4bd3313ef4da35a218d22d7a8e20c3e
Antonino Cucchiara <antonio_cuc@yahoo.co.uk>
2011-06-15 20:10:12 +0200

    Gpx track loader test.

---
+++ GpxSplitter-Source/test/gpxsplitter/tools/GpxTrackLoaderTest.java
@@ -0,0 +1,28 @@
+/**
+ * Builds a Gpx from a gpx track markup file and tests its features.
+ *
+ * @author Antonino Cucchiara
+ */
+package gpxsplitter.tools;
+
+import gpxsplitter.model.GpxType;
+
+/**
+ *
+ * @author Antonio
+ */
+public final class GpxTrackLoaderTest extends GpxLoaderTest
+{
+
+    @Override
+    public String getData()
+    {
+        return TestMedia.getTestTrack();
+    }
+
+    @Override
+    public GpxType getExpectedType()
+    {
+        return GpxType.Track;
+    }
+}
