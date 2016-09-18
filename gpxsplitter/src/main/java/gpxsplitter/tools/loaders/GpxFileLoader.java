c7ba1e4a48deecf335f6650cbce69a3ea1481480
Antonio Cucchiara <antonio_cuc@yahoo.co.uk>
2016-09-18 09:08:49 +0100

    Check whether a gpx file has been opened before attempting to save it.

--- gpxsplitter/src/main/java/gpxsplitter/tools/loaders/GpxFileLoader.java
+++ gpxsplitter/src/main/java/gpxsplitter/tools/loaders/GpxFileLoader.java
@@ -17,7 +17,9 @@ import javax.xml.bind.Unmarshaller;
 public class GpxFileLoader implements GpxLoader {
 
     public static GpxDescriptor getType(GpxType gpxType) throws FileNotValidException {
-        if (gpxType.getTrk().size() > 0) {
+        if (gpxType == null) {
+            throw new FileNotValidException();
+        } else if (gpxType.getTrk().size() > 0) {
             return GpxDescriptor.Track;
         } else if (gpxType.getRte().size() > 0) {
             return GpxDescriptor.Route;
